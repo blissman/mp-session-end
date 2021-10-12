2eb1d6ba8d739bc0c3967d6dd03878526897f34c
Bliss Man <bliss.man@bellmedia.ca>
2021-10-12 13:44:56 -0400

    add mediaSessionStart event for reference

---
+++ mediaSessionStartEvent.js
@@ -0,0 +1,44 @@
+{
+    "source_request_id": "c0d6dd62-38f8-4f80-854b-3a84f3010cfe",
+    "mpid": "264200868161219981",
+    "timestamp_unixtime_ms": 1634060603370,
+    "environment": "development",
+    "events": [{
+        "event_type": "custom_event",
+        "data": {
+            "custom_event_type": "media",
+            "custom_flags": {},
+            "event_name": "Media Session Start",
+            "timestamp_unixtime_ms": 1634060600292,
+            "session_uuid": "CCEBA3DA-B2AA-4C80-BE3B-A34AC03BCE68",
+            "session_start_unixtime_ms": 1634060497521,
+            "custom_attributes": {
+                "content_title": "Funny Internet cat video",
+                "content_duration": 120000,
+                "content_id": "1234567",
+                "content_type": "Video",
+                "stream_type": "OnDemand",
+                "media_session_id": "f2c0cd0b-b733-4993-9b8d-0cbe98257c38"
+            },
+            "location": null,
+            "source_message_id": "312564d3-87a5-483d-b51c-7579668c7be7"
+        }
+    }],
+    "mp_deviceid": "ec6085b6-91fc-43c1-b895-d65bca4c3cf0",
+    "sdk_version": "2.14.1",
+    "application_info": {},
+    "device_info": {
+        "platform": "web",
+        "screen_width": 1920,
+        "screen_height": 1080
+    },
+    "user_attributes": {},
+    "user_identities": null,
+    "consent_state": null,
+    "integration_attributes": {},
+    "context": {
+        "data_plan": {
+            "plan_id": "red_data_plan"
+        }
+    }
+}
